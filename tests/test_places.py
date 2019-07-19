771cc81c23cf639c5edcf94e17cb191124698e9c
Carlos Torres <snotchstar@gmail.com>
2019-07-19 10:54:59 -0700

    Update changed test data
    It was a bad idea to hardcode restaurant info in tests. Need to update.

--- tests/test_places.py
+++ tests/test_places.py
@@ -159,7 +159,7 @@ class PlacesTests(unittest.TestCase):
         self.register()
         self.login()
         response = self.app.post('/search',
-                                 data=dict(searchTerm='the range',
+                                 data=dict(searchTerm='range bernalillo',
                                            zipCode=87004, radius=10),
                                  follow_redirects=True)
         self.assertIn(b'/addPlace/ChIJ95RxxRN4IocRUhvj7gXGxEo', response.data)
@@ -182,7 +182,7 @@ class PlacesTests(unittest.TestCase):
         self.app.post('/addPlace/ChIJ95RxxRN4IocRUhvj7gXGxEo',
                       follow_redirects=True)
         response = self.app.get('/', follow_redirects=True)
-        self.assertIn(b'The Range Cafe', response.data)
+        self.assertIn(b'Range Cafe Bernalillo', response.data)
 
     # maybe test GooglePlace attributes?
 
